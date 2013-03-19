9f551971e3f0c73197d9453b59a3f289aa7f6707
cesco89 <cescoriga@gmail.com>
2013-03-19 22:29:34 +0100

    changed layout and some stuff

--- gen/com/cesco/tetheringhack/R.java
+++ gen/com/cesco/tetheringhack/R.java
@@ -14,9 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int button1=0x7f07000d;
+        public static final int editInt=0x7f07000e;
+        public static final int editPort=0x7f070011;
+        public static final int menu_settings=0x7f070012;
         public static final int textView1=0x7f070000;
+        public static final int textView10=0x7f070009;
+        public static final int textView11=0x7f07000a;
+        public static final int textView12=0x7f07000b;
+        public static final int textView13=0x7f07000c;
+        public static final int textView14=0x7f07000f;
+        public static final int textView15=0x7f070010;
+        public static final int textView2=0x7f070001;
+        public static final int textView3=0x7f070002;
+        public static final int textView4=0x7f070003;
+        public static final int textView5=0x7f070004;
+        public static final int textView6=0x7f070005;
+        public static final int textView7=0x7f070006;
+        public static final int textView8=0x7f070007;
+        public static final int textView9=0x7f070008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/cesco/tetheringhack/MainActivity.java
+++ src/com/cesco/tetheringhack/MainActivity.java
@@ -5,25 +5,36 @@ import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.cesco.tetheringhack.util.*;
 
 public class MainActivity extends Activity {
 	Button button1;
-	
+	TextView editInt;
+	TextView port;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 		button1 = (Button) findViewById(R.id.button1);
+		final EditText Interface_text = (EditText) this.findViewById(R.id.editInt);
+		final EditText Port_text = (EditText) this.findViewById(R.id.editPort);
+		
 		OnClickListener oclButton1 = new OnClickListener() {
 			@SuppressWarnings("deprecation")
 			@Override
 		       public void onClick(View v) {
+				
 		    	   CMDProcessor cmd = new CMDProcessor();
-		    	   cmd.su.runWaitFor("busybox iptables -tnat -A natctrl_nat_POSTROUTING -s 192.168.43.0/24 -o rmnet0 -j MASQUERADE");	   
+		    	   cmd.su.runWaitFor("busybox iptables -tnat -A natctrl_nat_POSTROUTING -s 192.168.0.0/" + Port_text.getText().toString() + " -o " + Interface_text.getText().toString() + " -j MASQUERADE");	   
 		    	   Toast.makeText(getApplicationContext(), "Hack Enabled!", Toast.LENGTH_LONG).show();
+				
+		    	   //debug with Toast messages LOL! :D
+		    	   //Toast.makeText(getApplicationContext(),"busybox iptables -tnat -A natctrl_nat_POSTROUTING -s 192.168.0.0/" + Port_text.getText().toString() + " -o " + Interface_text.getText().toString() + " -j MASQUERADE" , Toast.LENGTH_LONG).show();
 	}
 		};
 		button1.setOnClickListener(oclButton1);
